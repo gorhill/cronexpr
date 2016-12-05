4a6b5478eec1a418dd427e9b84a1fcec6de317f0
xiaofan <wangxiaofan@xiaomi.com>
2016-12-05 11:12:22 +0800

    fix interval 60 issue

--- cronexpr_parse.go
+++ cronexpr_parse.go
@@ -473,6 +473,9 @@ func genericFieldParse(s string, desc *fieldDescriptor) ([]*cronDirective, error
 			directive.first = desc.min
 			directive.last = desc.max
 			directive.step = atoi(snormal[pairs[2]:pairs[3]])
+			if directive.step < 1 || directive.step > desc.max {
+				return nil, fmt.Errorf("invalid interval %s", snormal)
+			}
 			directives = append(directives, &directive)
 			continue
 		}
@@ -483,6 +486,9 @@ func genericFieldParse(s string, desc *fieldDescriptor) ([]*cronDirective, error
 			directive.first = desc.atoi(snormal[pairs[2]:pairs[3]])
 			directive.last = desc.max
 			directive.step = atoi(snormal[pairs[4]:pairs[5]])
+			if directive.step < 1 || directive.step > desc.max {
+				return nil, fmt.Errorf("invalid interval %s", snormal)
+			}
 			directives = append(directives, &directive)
 			continue
 		}
@@ -493,6 +499,9 @@ func genericFieldParse(s string, desc *fieldDescriptor) ([]*cronDirective, error
 			directive.first = desc.atoi(snormal[pairs[2]:pairs[3]])
 			directive.last = desc.atoi(snormal[pairs[4]:pairs[5]])
 			directive.step = atoi(snormal[pairs[6]:pairs[7]])
+			if directive.step < 1 || directive.step > desc.max {
+				return nil, fmt.Errorf("invalid interval %s", snormal)
+			}
 			directives = append(directives, &directive)
 			continue
 		}
